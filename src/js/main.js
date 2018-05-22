2bede7a5dcffb15cd4de33fbe13ca56f12f2e152
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-05-22 11:23:37 -0300

    toasty

--- src/js/main.js
+++ src/js/main.js
@@ -1,103 +1,103 @@
-function selectFile() {
-  document.getElementById('file-input').click();
+function selectFile () {
+  document.getElementById('file-input').click()
 }
 
-function onSelectFile() {
-  let name = document.getElementById('file-input');
+function onSelectFile () {
+  let name = document.getElementById('file-input')
   if (name.files && name.files.item(0)) {
-    openFile(name);
+    openFile(name)
   }
 }
 
-function openFile(file) {
-  img = new SimpleImage(file);
-  preloaderSpinner.style.display = '';
-  setTimeout(drawImage, 1000);
+function openFile (file) {
+  img = new SimpleImage(file)
+  preloaderSpinner.style.display = ''
+  setTimeout(drawImage, 1000)
 }
 
-function drawImage() {
-  img.drawTo(canvas);
-  preloaderSpinner.style.display = 'none';
-  canvas.style.display = '';
+function drawImage () {
+  M.toast({html: 'File opened'})
+  img.drawTo(canvas)
+  preloaderSpinner.style.display = 'none'
+  canvas.style.display = ''
 }
 
-function resamplePicture() {
-  let width = widthInput.value;
-  let height = heightInput.value;
-  console.log('Will resample to ' + width + 'x' + height + '.');
+function resamplePicture () {
+  let width = widthInput.value
+  let height = heightInput.value
+  console.log('Will resample to ' + width + 'x' + height + '.')
 }
 
-function onChangeMethod() {
-  let selectedMethod = this.options[this.selectedIndex].value;
+function onChangeMethod () {
+  let selectedMethod = this.options[this.selectedIndex].value
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  let elements = document.querySelectorAll('.fixed-action-btn');
+document.addEventListener('DOMContentLoaded', function () {
+  let elements = document.querySelectorAll('.fixed-action-btn')
   let instances = M.FloatingActionButton.init(elements, {
     direction: 'top',
     hoverEnabled: false,
-  });
-});
+  })
+})
 
-document.addEventListener('DOMContentLoaded', function() {
-  let elements = document.querySelectorAll('.modal');
+document.addEventListener('DOMContentLoaded', function () {
+  let elements = document.querySelectorAll('.modal')
   let instances = M.Modal.init(elements, {
     inDuration: 300,
-  });
-});
+  })
+})
 
-document.addEventListener('DOMContentLoaded', function() {
-  let elements = document.querySelectorAll('select');
-  let instances = M.FormSelect.init(elements, {});
-});
+document.addEventListener('DOMContentLoaded', function () {
+  let elements = document.querySelectorAll('select')
+  let instances = M.FormSelect.init(elements, {})
+})
 
-document.addEventListener('DOMContentLoaded', function() {
-  let elements = document.querySelectorAll('.tooltipped');
+document.addEventListener('DOMContentLoaded', function () {
+  let elements = document.querySelectorAll('.tooltipped')
   let instances = M.Tooltip.init(elements, {
     enterDelay: 300,
     delay: 50,
-  });
-});
+  })
+})
 
-document.addEventListener('DOMContentLoaded', function() {
-  preloaderSpinner = document.getElementById('preloader');
-  preloaderSpinner.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function () {
+  preloaderSpinner = document.getElementById('preloader')
+  preloaderSpinner.style.display = 'none'
 
-  canvas = document.getElementById('canvas-img');
-  canvas.style.display = 'none';
+  canvas = document.getElementById('canvas-img')
+  canvas.style.display = 'none'
 
-  fileSelector = document.getElementById('file-input');
-  fileSelector.style.display = 'none';
-  fileSelector.addEventListener('change', onSelectFile);
+  fileSelector = document.getElementById('file-input')
+  fileSelector.style.display = 'none'
+  fileSelector.addEventListener('change', onSelectFile)
 
-  widthInput = document.getElementById('width');
-  heightInput = document.getElementById('height');
+  widthInput = document.getElementById('width')
+  heightInput = document.getElementById('height')
 
-  resampleBtn = document.getElementById('resample');
-  resampleBtn.addEventListener('click', resamplePicture);
+  resampleBtn = document.getElementById('resample')
+  resampleBtn.addEventListener('click', resamplePicture)
 
-  methodCombo = document.getElementById('method');
-  methodCombo.addEventListener('change', onChangeMethod);
+  methodCombo = document.getElementById('method')
+  methodCombo.addEventListener('change', onChangeMethod)
 
-});
+})
 
-let preloaderSpinner = null;
+let preloaderSpinner = null
 
-let canvas = null;
+let canvas = null
 
-let fileSelector = null;
+let fileSelector = null
 
-let widthInput = null;
+let widthInput = null
 
-let heightInput = null;
+let heightInput = null
 
-let resampleBtn = null;
+let resampleBtn = null
 
-let img = null;
+let img = null
 
-let methodCombo = null;
+let methodCombo = null
 
-const KNN = 1;
+const KNN = 1
 
-const INTERPOLATION = 2;
+const INTERPOLATION = 2
