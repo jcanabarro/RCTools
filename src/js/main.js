96bc7bbfb09be9524eeb6790865ad17f42ce8ded
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-05-24 13:57:06 -0300

    I honestly wish I could remember what was going on here..

--- src/js/main.js
+++ src/js/main.js
@@ -12,29 +12,35 @@ function onSelectFile () {
 function openFile (file) {
   img = new SimpleImage(file)
   preloaderSpinner.style.display = ''
-  setTimeout(drawImage, 1000)
+  setTimeout(onImageOpened, 1000)
 }
 
 function drawImage () {
-  M.toast({html: 'File opened'})
   img.drawTo(canvas)
-  preloaderSpinner.style.display = 'none'
-  canvas.style.display = ''
 }
 
 function resamplePicture () {
   let width = widthInput.value
   let height = heightInput.value
-  console.log('Will resample to ' + width + 'x' + height + '.')
 }
 
 function onChangeMethod () {
   let selectedMethod = this.options[this.selectedIndex].value
 }
 
+function onImageOpened () {
+  drawImage()
+  M.toast({html: 'File opened.' + img.width + 'x' + img.height})
+  preloaderSpinner.style.display = 'none'
+  canvas.style.display = ''
+  downloadBtn.classList.remove('disabled')
+  undoAllBtn.classList.remove('disabled')
+  resampleBtn.classList.remove('disabled')
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   let elements = document.querySelectorAll('.fixed-action-btn')
-  let instances = M.FloatingActionButton.init(elements, {
+  M.FloatingActionButton.init(elements, {
     direction: 'top',
     hoverEnabled: false,
   })
@@ -42,62 +48,98 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
   let elements = document.querySelectorAll('.modal')
-  let instances = M.Modal.init(elements, {
+  M.Modal.init(elements, {
     inDuration: 300,
   })
 })
 
 document.addEventListener('DOMContentLoaded', function () {
   let elements = document.querySelectorAll('select')
-  let instances = M.FormSelect.init(elements, {})
+  M.FormSelect.init(elements, {})
 })
 
 document.addEventListener('DOMContentLoaded', function () {
   let elements = document.querySelectorAll('.tooltipped')
-  let instances = M.Tooltip.init(elements, {
+  M.Tooltip.init(elements, {
     enterDelay: 300,
     delay: 50,
   })
 })
 
 document.addEventListener('DOMContentLoaded', function () {
+  // UI
+
   preloaderSpinner = document.getElementById('preloader')
   preloaderSpinner.style.display = 'none'
 
   canvas = document.getElementById('canvas-img')
   canvas.style.display = 'none'
 
+  // File
+
   fileSelector = document.getElementById('file-input')
   fileSelector.style.display = 'none'
   fileSelector.addEventListener('change', onSelectFile)
 
+  // Resample
+
   widthInput = document.getElementById('width')
   heightInput = document.getElementById('height')
 
-  resampleBtn = document.getElementById('resample')
-  resampleBtn.addEventListener('click', resamplePicture)
+  confirmResampleBtn = document.getElementById('confirm-resample')
+  confirmResampleBtn.addEventListener('click', resamplePicture)
 
   methodCombo = document.getElementById('method')
   methodCombo.addEventListener('change', onChangeMethod)
 
+  // Menu
+
+  uploadBtn = document.getElementById('upload-btn')
+  downloadBtn = document.getElementById('download-btn')
+  undoAllBtn = document.getElementById('undo-btn')
+  aboutBtn = document.getElementById('about-btn')
+  resampleBtn = document.getElementById('resample-btn')
+
 })
+
+// General/UI
 
 let preloaderSpinner = null
 
 let canvas = null
 
-let fileSelector = null
+// Resample Inputs
 
 let widthInput = null
 
 let heightInput = null
 
+let methodCombo = null
+
+let confirmResampleBtn = null
+
+// Menu
+
+let uploadBtn = null
+
+let downloadBtn = null
+
+let undoAllBtn = null
+
 let resampleBtn = null
+
+let aboutBtn = null
+
+// Img
 
 let img = null
 
-let methodCombo = null
+// Method
 
 const KNN = 1
 
 const INTERPOLATION = 2
+
+// File
+
+let fileSelector = null
